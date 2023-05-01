5b14f4b1bb41fdf8d2d935aacb02e5fec81526b4
el-wiam <elfardwiam@gmail.com>
2023-05-01 17:35:52 +0100

    1

---
+++ projet python/front_end/top.py
@@ -0,0 +1,176 @@
+import customtkinter 
+
+class ToplevelWindow(customtkinter.CTkToplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.geometry("400x300")
+        window_width = 1000
+        window_height = 600
+        bg_color="black",
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        position_top = int(screen_height / 4 - window_height / 4)
+        position_right = int(screen_width / 2 - window_width / 2)
+        self.geometry(f'{window_width}x{window_height}+{position_right}+{position_top}')
+        self.config(bg="black")
+        self.title('sign in')
+
+        self.resizable(False, False)
+
+    def signIN(self):
+        # Nom
+        self.label_nom = customtkinter.CTkLabel(self, 
+                                            text="Nom : ",
+                                            bg_color="black")
+        self.label_nom.place(x=20, y=20)
+
+        self.nomEntry=customtkinter.CTkEntry(self,
+                                             bg_color="#3D404B",
+                                             fg_color="white",
+                                             width=200,
+                                             height=50)
+        self.nomEntry.place(x=120, y=20) 
+
+        # prenom 
+        self.label_prenom = customtkinter.CTkLabel(self, 
+                                            text="Prenom : ",
+                                            bg_color="black")
+        self.label_prenom.place(x=400, y=20)
+
+        self.prenomEntry=customtkinter.CTkEntry(self,
+                                             bg_color="#3D404B",
+                                             fg_color="white",
+                                             width=200,
+                                             height=50)
+        self.prenomEntry.place(x=500, y=20) 
+        # username 
+        self.label_username = customtkinter.CTkLabel(self, 
+                                            text="username : ",
+                                            bg_color="black")
+        self.label_username.place(x=20, y=100)
+
+        self.usernameEntry=customtkinter.CTkEntry(self,
+                                             bg_color="#3D404B",
+                                             fg_color="white",
+                                             width=200,
+                                             height=50)
+        self.usernameEntry.place(x=120, y=100) 
+        # CIN
+        self.label_CIN = customtkinter.CTkLabel(self, 
+                                            text="CIN : ",
+                                            bg_color="black")
+        self.label_CIN.place(x=400, y=100)
+
+        self.CINEntry=customtkinter.CTkEntry(self,
+                                             bg_color="#3D404B",
+                                             fg_color="white",
+                                             width=200,
+                                             height=50)
+        self.CINEntry.place(x=500, y=100) 
+
+        # Tele
+        self.label_Tele = customtkinter.CTkLabel(self, 
+                                            text="Telephone : ",
+                                            bg_color="black")
+        self.label_Tele.place(x=20, y=180)
+
+        self.TeleEntry=customtkinter.CTkEntry(self,
+                                             bg_color="#3D404B",
+                                             fg_color="white",
+                                             width=200,
+                                             height=50)
+        self.TeleEntry.place(x=120, y=180) 
+
+        # Email
+        self.label_Email = customtkinter.CTkLabel(self, 
+                                            text="Email : ",
+                                            bg_color="black")
+        self.label_Email.place(x=400, y=180)
+
+        self.EmailEntry=customtkinter.CTkEntry(self,
+                                             bg_color="#3D404B",
+                                             fg_color="white",
+                                             width=200,
+                                             height=50)
+        self.EmailEntry.place(x=500, y=180) 
+
+        # Password
+        self.label_Password = customtkinter.CTkLabel(self, 
+                                            text="Password : ",
+                                            bg_color="black")
+        self.label_Password.place(x=20, y=260)
+
+        self.PasswordEntry=customtkinter.CTkEntry(self,
+                                             bg_color="#3D404B",
+                                             fg_color="white",
+                                             width=200,
+                                             height=50)
+        self.PasswordEntry.place(x=120, y=260) 
+
+        # Confirm Password
+        self.label_ConfirmPassword = customtkinter.CTkLabel(self, 
+                                            text="ConfirmPassword : ",
+                                            bg_color="black")
+        self.label_ConfirmPassword.place(x=400, y=260)
+
+        self.ConfirmPasswordEntry=customtkinter.CTkEntry(self,
+                                             bg_color="#3D404B",
+                                             fg_color="white",
+                                             width=200,
+                                             height=50)
+        self.ConfirmPasswordEntry.place(x=500, y=260) 
+
+        # button
+        self.signButton = customtkinter.CTkButton(  self, 
+                                                fg_color='#FFED00', 
+                                                text='Sign UP', 
+                                                text_color="black",
+                                                bg_color='black',
+                                                width=256, 
+                                                height=45,
+                                                font=("yu gothic ui bold", 16 * -1),
+                                                cursor='hand2')
+        self.signButton.place(x=380, y=410)
+
+    def forgot_password(self):
+        # Email
+        self.label_username = customtkinter.CTkLabel(self, 
+                                            text="Username / email : ",
+                                            bg_color="black")
+        self.label_username.place(x=440, y=100)
+
+        self.username=customtkinter.CTkEntry(self,
+                                             bg_color="#3D404B",
+                                             fg_color="black",
+                                             width=200,
+                                             height=50
+                                             )
+        self.username.place(x=400, y=150) 
+
+        # password 
+        self.label_username = customtkinter.CTkLabel(self, 
+                                            text="Password : ",
+                                            bg_color="black")
+        self.label_username.place(x=470, y=250)
+
+        self.PasswordEntry=customtkinter.CTkEntry(self,
+                                             bg_color="#3D404B",
+                                             fg_color="black",
+                                             width=200,
+                                             height=50)
+        self.PasswordEntry.place(x=400, y=300 ) 
+
+        # button
+        self.update_pass = customtkinter.CTkButton(  self, 
+                                                fg_color='#FFED00', 
+                                                text='Update Password', 
+                                                text_color="black",
+                                                bg_color='black',
+                                                width=256, 
+                                                height=45,
+                                                font=("yu gothic ui bold", 16 * -1),
+                                                cursor='hand2')
+        self.update_pass.place(x=380, y=410)
+
+
+        
---
+++ projet python/front_end/toplevel.py
@@ -0,0 +1,156 @@
+from tkinter import *
+import customtkinter
+import top
+
+class App(customtkinter.CTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        height = 600
+        width = 1000
+        x = (self.winfo_screenwidth() // 2) - (width // 2)
+        y = (self.winfo_screenheight() // 4) - (height // 4)
+        self.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+        self.title("login")
+        self.config(bg='black')
+
+        self.Login_backgroundImage = PhotoImage(file="assets\\loginp.png")
+        self.bg_imageLogin = customtkinter.CTkLabel(
+            self,
+            image=self.Login_backgroundImage,
+            fg_color="black",
+            text=""
+        )
+        self.bg_imageLogin.pack(side="top", padx=100, pady=100)
+        
+        # ============================= GO TO SIGN UP =============================
+        # ================ Email Name Section ====================
+        # ICON email
+        self.Login_emailName_image = PhotoImage(file="assets\\iconeEmail.png")
+        self.Login_emailName_image_Label = customtkinter.CTkLabel(
+            self,
+            text="",
+            fg_color="black",
+            image=self.Login_emailName_image,
+        )
+        self.Login_emailName_image_Label.place(x=315,y=240)
+
+        # Label email name
+        self.Login_emailName_text = customtkinter.CTkLabel(
+            self,
+            text="Email account",
+            fg_color="black",
+            font=("yu gothic ui SemiBold", 13 * -1),
+        )
+        self.Login_emailName_text.place(x=350, y=240)
+
+        # input email
+        email=StringVar()
+        self.email_entry=customtkinter.CTkEntry(self,
+                                            textvariable=email,
+                                            width=300,
+                                            height=30,
+                                            corner_radius=30,
+                                            bg_color="black")
+        self.email_entry.place(x=350,y=290)
+        # ================ Email Name Section =======================
+        # *******************************************************************
+        # *******************************************************************
+        # ================ PASSWORD Name Section ====================
+
+        # icon password  
+        self.Login_pwdName_image = PhotoImage(file="assets\\iconPwd.png")
+        self.Login_pwdName_image_Label = customtkinter.CTkLabel(
+            self,
+            text="",
+            fg_color="black",
+            image=self.Login_pwdName_image,
+            bg_color="black"
+        )
+        self.Login_pwdName_image_Label.place(x=320, y=340)
+
+        # Label password name
+        self.Login_pwdName_text = customtkinter.CTkLabel(
+            self,
+            text="Password",
+            fg_color="black",
+            font=("yu gothic ui SemiBold", 13 * -1),
+        )
+        self.Login_pwdName_text.place(x=350, y=340)
+
+        # input password
+        pwd=StringVar()
+        self.pwd_Entry=customtkinter.CTkEntry(self,
+                                            textvariable=pwd,
+                                            width=300,
+                                            height=30,
+                                            corner_radius=30,
+                                            bg_color="black")
+        self.pwd_Entry.place(x=350,y=390)
+        # ================ PASSWORD Name Section ====================
+        # ================ Submit button ============================
+        self.Login_button = customtkinter.CTkButton(
+            self,
+            width=230,
+            height=40,
+            text="login",
+            bg_color="black",
+            cursor="hand2",
+            fg_color="#FFED00",
+            text_color="black",
+            font=("yu gothic ui Bold", 16 * -1),
+            corner_radius=20,
+        )
+        self.Login_button.place(x=390, y=500)
+
+        # ================ Submit button ============================
+        # ================ forget button ============================
+
+        self.forgotPassword = customtkinter.CTkButton(
+            self,
+            text="Forgot Password",
+            width=150, height=35,
+            text_color="#FFE601",
+            font=("yu gothic ui Bold", 13 * -1),
+            bg_color="black",
+            fg_color="black",
+            hover_color="black",
+            border_width=0,
+            cursor="hand2",
+            command=lambda: top.ToplevelWindow(self).forgot_password(),
+        )
+        self.forgotPassword.place(x=320, y=440)
+        # ================ forget button ============================
+        # ================ sign in button ============================
+        self.forgotPassword = customtkinter.CTkButton(
+            self,
+            text="Sign UP",
+            width=150, height=35,
+            text_color="#FFE601",
+            font=("yu gothic ui Bold", 13 * -1),
+            bg_color="black",
+            fg_color="black",
+            hover_color="black",
+            border_width=0,
+            cursor="hand2",
+            command=lambda: top.ToplevelWindow(self).signIN(),
+        )
+        self.forgotPassword.place(x=550, y=440)
+        # ================ sign in button ============================
+        # ============================= GO TO SIGN UP =============================
+
+        # self.button_1 = customtkinter.CTkButton(self, text="open toplevel", command=self.open_toplevel)
+        # self.button_1.pack(side="top", padx=20, pady=20)
+
+        self.toplevel_window = None
+
+    def open_toplevel(self):
+        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
+            self.toplevel_window = top.ToplevelWindow(self)  # create window if its None or destroyed
+        else:
+            self.toplevel_window.focus()  # if window exists focus it
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
+    

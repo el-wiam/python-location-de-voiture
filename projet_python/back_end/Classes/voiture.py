eee40f4f3aa52cd30edf6c4c59d23615caa45726
safaamigo <safaabatrahi7@gmail.com>
2023-05-05 14:14:03 +0100

    safaa9

--- projet_python/back_end/Classes/voiture.py
+++ projet_python/back_end/Classes/voiture.py
@@ -6,34 +6,21 @@ class Voiture:
         pass
 
     def creer(self):
-        voiture_model = modelVoiture.VoitureModel(
-            marque='',
-            modele='',
-            type_carburant='',
-            nb_places=0,
-            transmission=0,
-            prix_location=0,
-            disponibilité=True,
-            image=''
-        )
-        return voiture_model.creer_voiture()
+        return modelVoiture.VoitureModel.creer_voiture(self)
 
     def modifier(self):
         return modelVoiture.VoitureModel.modifier_voiture(self)
-    
+
     def supprimer(self,id):
         return modelVoiture.VoitureModel.supprimer_voiture(self,id)
     
-    # client
+    # # client
     
-    def creerClt(self):
-        return modelClient.ClientModel.creer_client(self)
+    # def creerClt(self):
+    #     return modelClient.ClientModel.creer_client(self)
     
-    def modifierClt(self,id):
-        return modelClient.ClientModel.modifier_client(self)
+    # def modifierClt(self,id):
+    #     return modelClient.ClientModel.modifier_client(self)
     
-    def supprimerClt(self,id):
-        return modelClient.ClientModel.supprimer_client(self,id)
-
-voiture_instance = Voiture()
-voiture_instance.creer()
+    # def supprimerClt(self,id):
+    #     return modelClient.ClientModel.supprimer_client(self,id)

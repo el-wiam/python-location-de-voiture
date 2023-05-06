70703447c77c8213efefda6839dd723fd6ee5cfc
el-wiam <elfardwiam@gmail.com>
2023-05-06 02:16:24 +0100

    63

--- projet_python/front_end/search.py
+++ projet_python/front_end/search.py
@@ -84,7 +84,7 @@ class ToplevelWindow(customtkinter.CTkToplevel):
                 transmission=row[4] 
                 prix_location=row[5]
                 disponibilite=row[7]               
-                txt.insert(END,f"Marque :{marque} - model :{model}  , type du carburant : {typecarburant} , transmission : {transmission} , prix de location: {prix_location}, desponibilté : {disponibilite}")
+                txt.insert(END,f"MARQUE :{marque} \n - MODEL :{model} \n , TYPE DE CARBURANT : {typecarburant} \n TRANSMISSION : {transmission} \n PRIX DE LOCATION: {prix_location} \n DESPONIBILTE : {disponibilite}")
                 txt.yview(END)
             self.modify.focus_set()
         self.buttn.configure(command=search)        
